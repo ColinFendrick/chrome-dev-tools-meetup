819655bfff37f4c8ee748fea00c10ef150ab7dfa
Derek Dyer <derek@digitalderek.com>
2017-03-22 07:13:59 -0400

    Adds a little JS for talk

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
   console.log('Hi there! I\'m a console.log()');
 });
+
+var x = "I'm a variable"
+
+x
+
+document.getElementById("do-something-button").addEventListener("click", displayDate);
+
+function displayDate() {
+    document.getElementById("show-it").innerHTML = Date();
+}
